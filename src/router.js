4243677b3fce103d813ecbf259f2f22e8a309a17
pernillaTherese <pernilla.t.andersson@gmail.com>
2022-05-02 19:31:23 +0200

    Add HomeView and QuizView to router.

--- src/router.js
+++ src/router.js
@@ -1,10 +1,20 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
+import HomeView from './views/HomeView.vue'
+import QuizView from './views/QuizView.vue'
 import TipBotView from './views/TipBotView.vue'
 
 export default createRouter({
   history: createWebHashHistory(),
   routes: [
+    {
+      component: HomeView,
+      path: './HomeView'
+    },
+    {
+      component: QuizView,
+      path: './QuizView'
+    },
     {
       component: TipBotView,
       path: '/TipBot'
