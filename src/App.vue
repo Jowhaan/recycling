34e7ffc366d95df20ea5d123466b03898eb9b7bd
pernillaTherese <pernilla.t.andersson@gmail.com>
2022-05-03 20:23:01 +0200

    Install Sass and Add how-to-use-comment in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <script>
+  import MenuList from './components/MenuList.vue'
   export default {
-    components: {}
+    components: { MenuList }
   }
 </script>
 
 <template>
+  <MenuList />
   <main>
     <RouterView />
   </main>
 </template>
+
+<!-- SASS för att kunna ändra Bootstrapelement samt kunna göra
+färger och andra stilsättningsinställningar till VARIABLER -->
+
+<!-- OM VI VILL HA SASS FÖR HELA PROJEKTET:
+Lägg det här i App.vue
+
+<style lang="scss"></style>
+-->
+
+<!-- OM VI VILL HA SASS FÖR ETT SPECIFIKT ELEMENT:
+Lägg in scoped style i elementet t.ex. så här:
+
+<style lang="scss" scoped>
+  $some-variable: bold;
+
+  p {
+    font-weight: $some-variable;
+  }
+</style>
+
+<template>
+  <p>Hejsan</p>
+</template>
+
+ -->
