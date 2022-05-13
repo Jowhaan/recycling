d21d7c966486aab7ca658bcd617880e9a90a94d2
Jowhaan <91131770+Jowhaan@users.noreply.github.com>
2022-05-13 15:24:07 +0200

    tweak functionality

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -8,6 +8,8 @@
         searchQuery: '',
         searchHit: false,
         allImagePaths: [],
+        specificImagePaths: [],
+        containsSpecific: false,
         currentSign: [],
         signClicked: false,
         signClickedPath: '',
@@ -19,6 +21,7 @@
     watch: {
       searchQuery() {
         this.searchHit = false
+        this.getSpecificImagePaths()
         this.signClicked = false
         this.currentTrashObject = null
         this.$store.state.trash.filter((result) => {
@@ -31,6 +34,16 @@
       }
     },
     methods: {
+      getSpecificImagePaths() {
+        this.containsSpecific = false
+        this.specificImagePaths = []
+        this.$store.state.trash.filter((result) => {
+          if (result.engName.startsWith(this.searchQuery)) {
+            this.specificImagePaths.push(result.sign)
+            this.containsSpecific = true
+          }
+        })
+      },
       getAllImagePaths() {
         for (var i = 0; i < this.$store.state.trash.length; i++) {
           var currentPath = this.$store.state.trash[i].sign
@@ -48,6 +61,7 @@
       },
       onSignClick(imagePath) {
         this.currentSign = []
+        this.searchBarFocused = false
         this.signClicked = true
         this.signClickedPath = imagePath
         var test = this.$store.state.trash.filter(
@@ -100,12 +114,25 @@
   <div class="flexbox-container">
     <h1 class="flexbox-item-1">What do you want to recycle?</h1>
     <input
-      class="flexbox-item-input"
+      @focus="isFocused"
+      class="flexbox-item-input form-control"
+      list="datalistOptions"
       type="text"
       v-model="searchQuery"
       placeholder="Ex. toothbrush, playstation..."
       aria-label="Search"
     />
+    <datalist id="datalistOptions">
+      <option value="Detta är statiska värden" />
+      <option value="Som ska ersättas med en 'v-for'" />
+      <option value="Som ska leda till en array" />
+      <option value="Som ska innehålla matchande ord på" />
+      <option value="Det man har börjat söka på" />
+      <option value="Som jag kanske redan har någonstans" />
+      <option value="I min kod men jag har gjort tillräckligt" />
+      <option value="För idag, så nu tar vi helg" />
+      <option value="Sen ska listan bli dynamisk" />
+    </datalist>
     <div v-if="searchHit && !signClicked">
       <img
         class="trashSign"
@@ -114,7 +141,7 @@
       <h3>You recycle your {{ this.searchQuery }} here</h3>
       <p>{{ this.currentTrashObject.whatHappens }}</p>
     </div>
-    <div v-if="searchQuery && !searchHit && !signClicked">
+    <div v-if="searchQuery && !containsSpecific && !searchHit && !signClicked">
       <h3>Categories:</h3>
       <div
         :key="this.allImagePaths[index]"
@@ -143,6 +170,21 @@
           {{ trashName }}
         </li>
       </ul>
+    </div>
+    <div v-if="searchQuery && containsSpecific && !signClicked">
+      <h3>Categories:</h3>
+      <div
+        :key="this.specificImagePaths[index]"
+        class="image-items"
+        v-for="(imagePath, index) in specificImagePaths"
+      >
+        <img
+          class="trashSign"
+          :src="'../../assets/RecyclingSigns/' + imagePath + '.svg'"
+          :alt="imagePath"
+          @click="onSignClick(imagePath)"
+        />
+      </div>
     </div>
   </div>
 </template>
