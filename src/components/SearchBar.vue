f2107212c23c50c20d00a46ec403274a59d1c15b
LinusObermayr <linus.obermayr@hotmail.com>
2022-05-16 10:30:04 +0200

    bla bla

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,5 +1,22 @@
 <script>
-  export default {}
+  export default {
+    data() {
+      return {
+        searchText: 0
+      }
+    },
+    methods: {
+      getResult() {
+        let result = ''
+        for (let i = 0; i < this.$store.state.trash.length; i++) {
+          if (this.$store.state.trash[i].engName.includes(this.searchText)) {
+            result += this.$store.state.trash[i]
+          }
+        }
+        return result
+      }
+    }
+  }
 </script>
 
 <style>
@@ -12,22 +29,30 @@
   .flexbox-item-1 {
     font-style: Italic;
     font-weight: bold;
-    font-size: 25px;
+    font-size: 20px;
+    width: 80%;
     padding-bottom: 20px;
     padding-top: 150px;
     flex-grow: 1;
-    align-items: center;
+    text-align: center;
   }
   .flexbox-item-input {
-    border: 3px solid black;
-    height: 20px;
+    border: 3px solid rgb(78, 211, 21);
+    height: 50px;
+    width: 70%;
+    max-width: 450px;
     border-radius: 25px;
     flex-grow: 1;
     align-items: center;
   }
 
   body {
-    background-color: rgb(54, 144, 36);
+    background-color: rgb(249, 249, 249);
+  }
+
+  ::placeholder {
+    padding-left: 5px;
+    color: black;
   }
 </style>
 
@@ -35,12 +60,16 @@
   <body>
     <div class="flexbox-container">
       <div class="flexbox-item-1">What do you want to recycle?</div>
+
       <input
         class="flexbox-item-input"
+        v-model="searchText"
         type="text"
         placeholder="Ex. toothbrush, playstation..."
         aria-label="Search"
       />
+      <i class="bi bi-search" type="button" @onClick="getResult" />
+      <div class="listgroup">hej</div>
     </div>
   </body>
 </template>
