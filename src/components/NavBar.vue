4e929b05631043eb626dc85d4e39218ef6c53cb1
LinusObermayr <linus.obermayr@hotmail.com>
2022-05-23 14:14:25 +0200

    ändrat login-knapp

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -57,9 +57,9 @@
       />
     </RouterLink>
     <span v-if="this.$store.state.loggedIn">
-      <button class="btnlog" @click="logout()">Logout</button>
+      <p class="btnlog" @click="logout()">Logout</p>
     </span>
-    <span v-else><button class="btnlog" @click="goToLog()">Login</button></span>
+    <span v-else><p class="btnlog" @click="goToLog()">Login</p></span>
   </nav>
 </template>
 
@@ -131,21 +131,8 @@
     }
   }
   .btnlog {
-    border-radius: 5px;
-    border-color: #0b604d;
     margin-right: 15px;
-    text-align: center;
-    font-size: 15px;
-    font-style: bold;
-  }
-  .btnlog:hover {
-    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
-      0 17px 50px 0 rgba(0, 0, 0, 0.19);
-  }
-  .flexbox-container2 {
-    display: flex;
-    justify-content: space-evenly;
-    flex-direction: row;
+    margin-top: 15px;
   }
 
   .navbar {
@@ -153,10 +140,10 @@
     justify-content: space-between;
     width: 100%;
     padding-left: 15px;
+    align-items: center;
   }
 
   .image {
-    align-items: center;
     padding-left: 10px;
     margin-left: 15px;
   }
